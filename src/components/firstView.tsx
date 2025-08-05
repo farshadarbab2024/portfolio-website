7910879cc7895a523c1961ca8c0f9cc9ae98303c
Your Name <your-email@example.com>
2025-08-05 09:48:41 +0330

    chert

--- src/components/firstView.tsx
+++ src/components/firstView.tsx
@@ -24,14 +24,7 @@ function FirstView() {
         <div className="z-10 absolute top-1/2 right-1/2 translate-x-1/2
         -translate-y-1/2 w-[85vw]">
 
-          <div //picture circle
-          className="w-[60vw] h-[60vw] mx-auto flex justify-center items-center"
-          id="picture_circle">
-            <img src={process.env.PUBLIC_URL + "/images/Farshad Arbab profile picture.png" }
-            alt="تصویر پروفایل فرشاد ارباب"
-            className="rounded-full w-[93%] h-[93%]"
-            />
-          </div>
+
 
           <h1 className="block text-center font-black color-black text-[8vw] mt-8" >{fullName}</h1>
           <span className="block text-center text-[#888888] text-[4.3vw]">{jobTitle}</span>
